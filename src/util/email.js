b4aa5a46171b33c937334216f81682bbffced15f
黄斌杰 <501711499@qq.com>
2014-08-16 14:44:42 +0800

    delete mail

--- src/util/email.js
+++ src/util/email.js
@@ -2,6 +2,5 @@
  * Created by hbj on 2014/8/16.
  */
 var nodemailer = require('nodemailer');
-var ses = require('nodemailer-ses-transport');
 var transporter = nodemailer.createTransport();
 module.exports.transporter = transporter;
